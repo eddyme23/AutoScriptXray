ee516d6bc3a8f15d3b37731e2d5a5ed298b93abb
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-06-06 14:08:15 +0700

    x

--- menu/m-system.sh
+++ menu/m-system.sh
@@ -12,6 +12,7 @@ echo -e " [\e[36m•3\e[0m] Set Auto Reboot"
 echo -e " [\e[36m•4\e[0m] Restart All Service"
 echo -e " [\e[36m•5\e[0m] Cek Bandwith"
 echo -e " [\e[36m•6\e[0m] Install TCP BBR"
+echo -e " [\e[36m•7\e[0m] DNS CHANGER"
 echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e   ""
@@ -28,6 +29,7 @@ case $opt in
 4) clear ; restart ; exit ;;
 5) clear ; bw ; exit ;;
 6) clear ; m-tcp ; exit ;;
+7) clear ; m-dns ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo -e "" ; echo "Anda salah tekan" ; sleep 1 ; m-system ;;
