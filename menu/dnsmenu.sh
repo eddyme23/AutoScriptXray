49dd61211dde9337ddb6b79657391556fb7769cc
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-06-06 13:54:55 +0700

    x

---
+++ menu/dnsmenu.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+# =========================================
+# Auther  : givpn
+# =========================================
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+y="\033[0;1;37m"
+wh="\033[0m"
+# Getting
+MYIP=$(wget -qO- ipv4.icanhazip.com);
+echo "Checking VPS"
+clear
+echo -e "$y            DNS CHANGER$wh"
+echo -e "======================================"
+dnsfile="/root/dns"
+if test -f "$dnsfile"; then
+udns=$(cat /root/dns)
+echo -e ""
+echo -e "Active DNS : $udns"
+fi
+echo -e ""
+echo -e "[1]. CHANGE DNS"
+echo -e "[2]. Reset DNS To Default"
+echo -e "[3]. Back To Main Menu"
+echo -e ""
+read -p "Select From Options [ 1 - 3 ] :  " dns
+echo -e ""
+echo -e "======================================"
+case $dns in
+1)
+clear
+echo ""
+read -p "Please Insert DNS : " dns1
+if [ -z $dns1 ]; then
+echo ""
+echo "Please Insert DNS !"
+sleep 1
+clear
+dns
+fi
+rm /etc/resolv.conf
+rm /etc/resolvconf/resolv.conf.d/head
+touch /etc/resolv.conf
+touch /etc/resolvconf/resolv.conf.d/head
+echo "$dns1" > /root/dns
+echo "nameserver $dns1" >> /etc/resolv.conf
+echo "nameserver $dns1" >> /etc/resolvconf/resolv.conf.d/head
+systemctl restart resolvconf.service
+echo ""
+echo -e "\e[032;1mDNS $dns1 sucessfully insert in VPS\e[0m"
+echo ""
+cat /etc/resolvconf/resolv.conf.d/head
+sleep 1
+clear
+dns
+;;
+2)
+clear
+echo ""
+read -p "Reset To Default DNS [Y/N]: " -e answer
+if [ "$answer" = 'y' ] || [ "$answer" = 'Y' ]; then
+rm /root/dns
+echo ""
+echo -e "[ ${BLUE}INFO${NC} ] Delete Resolv.conf DNS"
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+sleep 1
+echo -e "[ ${BLUE}INFO${NC} ] Delete Resolv.conf.d/head DNS"
+echo "nameserver 8.8.8.8" > /etc/resolvconf/resolv.conf.d/head
+sleep 1
+else if [ "$answer" = 'n' ] || [ "$answer" = 'N' ]; then
+echo -e ""
+echo -e "[ ${BLUE}INFO${NC} ]  Operation Cancelled By User"
+sleep 1
+fi
+fi
+clear
+dns
+;;
+3)
+clear
+menu
+;;
+*)
+echo "Please enter an correct number"
+clear
+dns
+;;
+esac
