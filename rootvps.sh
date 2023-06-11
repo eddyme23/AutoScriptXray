29b4a8eba3c5c242e8f37c8d0a5d0ab3b09dd405
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-06-11 14:30:33 +0700

    x

--- rootvps.sh
+++ rootvps.sh
@@ -1,6 +1,13 @@
 # by givpn
 # ipinfo
 MYIP=$(wget -qO- ipinfo.io/ip);
+# pewarna hidup
+Bred="\e[1;31m"
+BGreen='\e[1;32m'
+BYellow='\e[1;33m'
+BBlue='\e[1;34m'
+BPurple='\e[1;35m'
+NC='\e[0m'
 wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/givpn/autoscriptxray/master/sshd_config;
 systemctl restart sshd;
 clear;
@@ -11,14 +18,14 @@ clear;
 printf "Please Save This VPS Account Information"
 if
 echo ""
-echo -e "Please Save This VPS Account Information"
-echo -e "---------------------------------------------"
-echo -e "Root Account (Main Account)"
-echo -e "Ip address = $MYIP"
-echo -e "Port       = 22 or 657"
-echo -e "Username   = root"
-echo -e "Password   = $pwe"
-echo -e "---------------------------------------------"
+echo -e "\e[1;31m Please Save This VPS Account Information\e[0m"
+echo -e "\e[1;34m------------------------------------------\e[0m"
+echo -e "\e[1;34mRoot Account (Main Account)\e[0m"
+echo -e "\e[1;34mIp address \e[0m = $MYIP"
+echo -e "\e[1;34mPort       \e[0m = 22 or 657"
+echo -e "\e[1;34mUsername   \e[0m = root"
+echo -e "\e[1;34mPassword   \e[0m = $pwe"
+echo -e "\e[1;34m-------------------------------------------"
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 exit
