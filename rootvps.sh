7335bea09e9ad0dd71d7a289e994de6432d98d2b
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-06-11 14:22:47 +0700

    x

--- rootvps.sh
+++ rootvps.sh
@@ -1,4 +1,6 @@
 # by givpn
+# ipinfo
+MYIP=$(wget -qO- ipinfo.io/ip);
 wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/givpn/autoscriptxray/master/sshd_config;
 systemctl restart sshd;
 clear;
@@ -6,13 +8,18 @@ echo -e "Enter Password:";
 read -e pwe;
 usermod -p `perl -e "print crypt("$pwe","Q4")"` root;
 clear;
-printf "Please Save This VPS Account Information
----------------------------------------------
-Root Account (Main Account)
-Ip address = $(curl -Ls http://ipinfo.io/ip)
-Port       = 22
-Username   = root
-Password   = $pwe
----------------------------------------------
-echo "";
-exit;
+printf "Please Save This VPS Account Information"
+if
+echo ""
+echo -e "Please Save This VPS Account Information"
+echo -e "---------------------------------------------"
+echo -e "Root Account (Main Account)"
+echo -e "Ip address = $MYIP"
+echo -e "Port       = 22 or 657"
+echo -e "Username   = root"
+echo -e "Password   = $pwe"
+echo -e "---------------------------------------------"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+exit
+fi
