3cd95bc719f552de26f08f67ddc9b7f4b4474c9a
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-06-12 11:12:56 +0700

    x

--- setup.sh
+++ setup.sh
@@ -1,52 +1,4 @@
 #!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
-
-BURIQ () {
-    curl -sS https://ipinfo.io/ip > /root/tmp
-    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
-    for user in "${data[@]}"
-    do
-    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
-    d1=(`date -d "$exp" +%s`)
-    d2=(`date -d "$biji" +%s`)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-    echo $user > /etc/.$user.ini
-    else
-    rm -f  /etc/.$user.ini > /dev/null 2>&1
-    fi
-    done
-    rm -f  /root/tmp
-}
-MYIP=$(curl -sS ipinfo.io/ip)
-Name=$givpn
-echo $Name > /usr/local/etc/.$Name.ini
-CekOne=$(cat /usr/local/etc/.$Name.ini)
-
-Bloman () {
-if [ -f "/etc/.$Name.ini" ]; then
-CekTwo=$(cat /etc/.$Name.ini)
-    if [ "$CekOne" = "$CekTwo" ]; then
-        res="Expired"
-    fi
-else
-res="Permission Accepted..."
-fi
-}
-
-PERMISSION () {
-    MYIP=$(curl -sS ipinfo.io/ip)
-    IZIN=$(curl -sS ipinfo.io/ip)
-    if [ "$MYIP" = "$IZIN" ]; then
-    Bloman
-    else
-    res="Permission Denied!"
-    fi
-    BURIQ
-}
-
 clear
 red='\e[1;31m'
 green='\e[0;32m'
@@ -148,7 +100,7 @@ sleep 0.5
 echo -ne "[ ${BGreen}INFO${NC} ] Check permission : "
 
 PERMISSION
-if [ -f /home/needupdate ]; then
+if
 BGreen "Your script need to update first !"
 exit 0
 elif [ "$res" = "Permission Accepted..." ]; then
