4e34649eb911ee979af94ac412c38406cc7499a1
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-06-16 11:28:56 +0700

    x

--- menu/m-vless.sh
+++ menu/m-vless.sh
@@ -11,6 +11,7 @@ echo -e " [\e[36m•2\e[0m] Trial Account Vless "
 echo -e " [\e[36m•3\e[0m] Extending Account Vless "
 echo -e " [\e[36m•4\e[0m] Delete Account Vless "
 echo -e " [\e[36m•5\e[0m] Check User Login Vless "
+echo -e " [\e[36m•6\e[0m] User list created Account "
 echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e ""
@@ -26,6 +27,7 @@ case $opt in
 3) clear ; renew-vless ; exit ;;
 4) clear ; del-vless ; exit ;;
 5) clear ; cek-vless ; exit ;;
+6) clear ; cat /etc/log-create-user.log ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; m-sshovpn ;;
