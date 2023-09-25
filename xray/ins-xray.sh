0c5391d0afdf1aeb69ee9c7159e8ad20f386772b
Sudah Bisa <127005370+rinandaerfan@users.noreply.github.com>
2023-09-26 03:11:33 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -53,7 +53,7 @@ touch /var/log/xray/error.log
 touch /var/log/xray/access2.log
 touch /var/log/xray/error2.log
 # / / Ambil Xray Core Version Terbaru
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.6.1
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.8.4
 
 ## crt xray
 systemctl stop nginx
